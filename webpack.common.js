f4679638f157c9851f9f2b2f7d49e8551dfb8909
MrZero Day <CopyCoding25>
2020-07-31 09:05:54 +0800

    inisialisasi project covid 19 landing page

---
+++ webpack.common.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.(png|jpg|svg)$/,
+                use: [{
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[hash].[ext]",
+                        outputPath: "assets/images"
+                    }
+                }]
+            },
+            {
+                test: /\.(svg|eot|ttf)$/,
+                use: [{
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[ext]",
+                        outputPath: "assets/fonts"
+                    }
+                }]
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html"
+        })
+    ]
+}
